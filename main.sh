a6055a51a7fbd7388ee6e38556566bdf48c3d077
flowing-abyss <flowing.abyss@gmail.com>
2025-07-30 20:32:56 +0700

    refactor: rename audio file variables
    Rename `AUDIO_FILE` to `RAW_AUDIO_FILE` and `COMPRESSED_AUDIO_FILE` to `FLAC_AUDIO_FILE` for improved clarity and consistency. This change makes the variable names more descriptive of their content and format.

--- main.sh
+++ main.sh
@@ -4,8 +4,9 @@ set -e
 
 SCRIPT_DIR=$(dirname "$0")
 TMP_DIR="/tmp"
-AUDIO_FILE="${TMP_DIR}/input.wav"
-COMPRESSED_AUDIO_FILE="${TMP_DIR}/input.flac"
+RAW_AUDIO_FILE="${TMP_DIR}/input.wav"
+FLAC_AUDIO_FILE="${TMP_DIR}/input.flac"
+
 API_KEY=$(cat "${SCRIPT_DIR}/.api")
 MODEL="whisper-large-v3-turbo"
 
@@ -19,23 +20,24 @@ if pgrep arecord; then
     sleep 0.2s
     mpg123 $END_AUDIO >/dev/null 2>&1 &
 
-    ffmpeg -i "$AUDIO_FILE" -c:a flac -compression_level 8 "$COMPRESSED_AUDIO_FILE" >/dev/null 2>&1
+    ffmpeg -i "$RAW_AUDIO_FILE" -c:a flac -compression_level 8 "$FLAC_AUDIO_FILE" >/dev/null 2>&1
 
     notify-send "💬 Speech recognition"
     output=$(curl -s https://api.groq.com/openai/v1/audio/transcriptions \
         -H "Authorization: Bearer ${API_KEY}" \
         -H "Content-Type: multipart/form-data" \
-        -F file="@${COMPRESSED_AUDIO_FILE}" \
+        -F file="@${FLAC_AUDIO_FILE}" \
         -F model="${MODEL}")
     text=$(echo "$output" | jq -r '.text')
 
     wl-copy $text
+    notify-send "📋 Sent to clipboard"
     sleep 0.1s
     hyprctl dispatch sendshortcut "CTRL,V,"
-    notify-send "📋 Sent to clipboard"
-    rm "$AUDIO_FILE" "$COMPRESSED_AUDIO_FILE"
+
+    rm "$RAW_AUDIO_FILE" "$FLAC_AUDIO_FILE"
 else
     mpg123 $START_AUDIO >/dev/null 2>&1 &
-    arecord --format S16_LE --rate=16000 "$AUDIO_FILE" >/dev/null 2>&1 &
+    arecord --format S16_LE --rate=16000 "$RAW_AUDIO_FILE" >/dev/null 2>&1 &
     notify-send "🔴 Start recording"
 fi
